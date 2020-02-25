eb5cd419464491d2f5dd002744ce6284a416ca0f
drac0-malf0y <malfoys76@gmail.com>
2020-02-25 15:39:05 +0530

    shit's coming

--- Activity.java
+++ Activity.java
@@ -1,33 +1,20 @@
-import java.util.*;
+class GFG { 
+// Function to print all sub strings 
+    static void subString(char str[], int n) { 
+        for (int len = 1; len <= n; len++)
 
-class Activity{
-	public static void main(String []args){
-		int []arr = {4,5,2,6,1};
-		int []brr = {2,8,1,3,5};
-		int size = brr.length;
+            for (int i = 0; i <= n - len; i++) {  
 
-		int arr1[] = new int[5];
-
-		for(int i = 0; i < size; i++){
-			int gasAvailable = arr[i];
-			int gasConsumed = brr[i];
-			int count = 0;
-
-			while(gasAvailable > gasConsumed){
-				gasAvailable = gasAvailable + arr[i+1] - brr[i];
-				gasConsumed = gasConsumed + brr[i+1];
-				i++;
-				count++;
-				if (count == 5){
-					arr1[i] = brr[i];
-				}
-			}
-			
-
-		}
-		Arrays.sort(arr1);
-		for(int i =0;i<=4;i++){
-		System.out.print(arr1[i]);
-}
-	}
+                for (int k = i; k <= i + len - 1; k++)
+                    System.out.print(str[k]); 
+                System.out.println(); 
+            } 
+    } 
+  
+// Driver program to test above function 
+    public static void main(String[] args) { 
+        char str[] = {'a', 'b', 'c'}; 
+        subString(str, str.length); 
+      
+    } 
 }
